854583ab8c9fa3c64776a535f2fb10fc4bdba48d
Tanmoy biswas <email2tanmoy@gmail.com>
2020-03-06 14:04:53 +0530

    06.03.2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,49 @@
 import { Component } from '@angular/core';
-import {EmpListService} from './emp-list.service';
+import { EmpListService } from './emp-list.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title:string = 'code-with-mosh-Angular';
-  public imgUrl:string="https://picsum.photos/200/300";
-  public colSpan:number= 2
-  EmpList;
+  public title: string = 'code-with-mosh-Angular';
+  public imgUrl: string = "https://picsum.photos/200/300";
+  public colSpan: number = 2
+  public EmpList;
   isActive = true;
+  public TwoWayDataBinding: String = "using typical way";
+  public TwoWayDataBindingngModel: string = "using ngmodel";
 
-  constructor(fetchEmpList:EmpListService){
+  public cource = {
+    title: "The Complete Angular Cource",
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  }
+
+  constructor(fetchEmpList: EmpListService) {
     this.EmpList = fetchEmpList.getEmpList();
   }
 
-  consoleMsg($event){
+  consoleMsg($event) {
     $event.stopPropagation();
     console.log("button was clicked", $event);
   }
-  onclickDiv(){
+  onclickDiv() {
     console.log("div was clicked");
+  }
+  onkeyUp($event) {
+    console.log($event.target.value);
+  }
+  onkeyUp1(email) {
+    console.log("using Template Veriable----------->>>>", email)
+  }
+  sampleMethod() {
+
+    console.log(this.TwoWayDataBinding);
+  }
+  sampleMethod1() {
+    console.log(this.TwoWayDataBindingngModel);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 
@@ -11,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
